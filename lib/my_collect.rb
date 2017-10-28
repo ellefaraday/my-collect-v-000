a9f19bb2fb078939d6228cee7a2240f22e0a4ba1
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 21:22:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,8 @@
 def my_collect(array)
   i = 0
-  new_collection = Array.new 
+  new_collection = Array.new
   while i < array.length
     new_collection << yield(array[i])
   end
   new_collection
 end
-
-
